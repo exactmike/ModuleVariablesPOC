770f94ee5bb49b85ad49b36b815bf747178e2828
Mike Campbell <exactmike@users.noreply.github.com>
2018-10-17 14:49:52 -0400

    Update ModuleVariablesPOC.psm1

--- ModuleVariablesPOC.psm1
+++ ModuleVariablesPOC.psm1
@@ -48,37 +48,3 @@ function Remove-ModuleVariable
     )
     Remove-Variable -Scope Script -Name $name
 }
-function Get-FirstNonNullEmptyStringVariableValueFromScopeHierarchy
-{
-    Param(
-        [string]$VariableName
-        ,
-        [int]$ScopeLevels = 15
-        ,
-        [int]$timeout = 500 #In Milliseconds
-    )
-    $scope = 0
-    $stopwatch = [Diagnostics.Stopwatch]::StartNew()
-    do
-    {
-        Try
-        {
-            $value = Get-Variable -Name $VariableName -ValueOnly -Scope $scope -ErrorAction SilentlyContinue
-        }
-        Catch
-        {
-        }
-        $scope++
-    }
-    until (-not [string]::IsNullOrWhiteSpace($value) -or $stopwatch.ElapsedMilliseconds -ge $timeout -or $scope -ge $ScopeLevels)
-    $value
-}
-#end function Get-FirstNonNullEmptyStringVariableValueFromScopeHierarchy
-###############################################################################################
-function Test-WriteLog
-{
-    #test if an outside function called within a module function can see the module variable.
-    #Yes, it can.  Of course, after thinking about it, just like you can use any external to a module powershell command within your module.
-    Write-Log -Message $MyIntVariable -Verbose
-}
-Export-ModuleMember -Variable MyIntVariable
